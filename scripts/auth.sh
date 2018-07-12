0cc8a44637dc6715c47ba296db398605ea552f8e
Maxime Lebastard <maxime@pathmotion.com>
2018-07-12 11:14:37 +0200

    Check the secret is defined

--- scripts/auth.sh
+++ scripts/auth.sh
@@ -1,3 +1,5 @@
+[ -z "$GCLOUD_SERVICE_ACCOUNT" ] && echo "Please set and expose the secret \"GCLOUD_SERVICE_ACCOUNT\"" && exit 1;
+
 printf "%s" "${GCLOUD_SERVICE_ACCOUNT}" > /google-service-account.json
 
 gcloud auth activate-service-account --key-file=/google-service-account.json
